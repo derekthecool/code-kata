5eecea49f5a2d15c18d6b97361c072546024424e
derekthecool <derekthecool@gmail.com>
2022-02-06 14:08:56 -0700

    Basic project setup

--- csharp/StringCalculator/StringCalculator_Library/Class1.cs
+++ csharp/StringCalculator/StringCalculator_Library/Class1.cs
@@ -1,5 +1,9 @@
 ﻿namespace StringCalculator_Library;
 public class Class1
 {
+    public static int Sum(string string1, string string2)
+    {
+        return 0;
+    }
 
 }
--- csharp/StringCalculator/StringCalculator_Test/UnitTest1.cs
+++ csharp/StringCalculator/StringCalculator_Test/UnitTest1.cs
@@ -1,12 +1,15 @@
 using Xunit;
+using StringCalculator_Library;
 
 namespace StringCalculator_Test;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void NullString()
     {
+        int dog = Class1.Sum("", "");
 
+        Assert.Equal(0, dog);
     }
 }
