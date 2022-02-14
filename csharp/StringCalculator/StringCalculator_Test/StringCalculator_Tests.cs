f8a0d27b38d947fa3679928fa96b2ed6a3cae116
derekthecool <derekthecool@gmail.com>
2022-02-14 10:49:30 -0700

    Setting better start point for string calculator

--- csharp/StringCalculator/StringCalculator_Test/StringCalculator_Tests.cs
+++ csharp/StringCalculator/StringCalculator_Test/StringCalculator_Tests.cs
@@ -5,14 +5,15 @@ namespace StringCalculator_Test;
 
 public class StringCalculator_Tests
 {
-    [Fact]
-    public void NullString()
+    [Theory]
+    [InlineData("1")]
+    public void SumTest(string input)
     {
         // Act
         StringCalculator SC = new StringCalculator();
 
         // Arrange
-        int result = SC.Sum("");
+        int result = SC.Sum(input);
 
         // Assert
         Assert.Equal(0, result);
