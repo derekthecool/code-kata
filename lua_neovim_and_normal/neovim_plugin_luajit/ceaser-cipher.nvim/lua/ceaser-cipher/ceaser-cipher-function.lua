050113fa91ca7a3c2c8af2ccac34d7dc913e09f2
derekthecool <derekthecool@gmail.com>
2022-09-22 09:20:47 -0600

    Long running work on the ceaser-cipher
    I'm a bit stuck at the moment

--- lua_neovim_and_normal/neovim_plugin_luajit/ceaser-cipher.nvim/lua/ceaser-cipher/ceaser-cipher-function.lua
+++ lua_neovim_and_normal/neovim_plugin_luajit/ceaser-cipher.nvim/lua/ceaser-cipher/ceaser-cipher-function.lua
@@ -1,7 +1,18 @@
 local M = {}
 
-M.ceaser_cipher = function(letter)
-  return letter
+M.ceaser_cipher = function(word, shift)
+	local output_word = ""
+	for letter in string.gmatch(word, ".") do
+		local letter_number = string.byte(letter:upper())
+		if letter_number + shift > string.byte("Z") then
+			letter_number = letter_number - (26 - shift)
+		else
+			letter_number = letter_number + shift
+		end
+		output_word = output_word .. letter string.char(letter_number)
+	end
+
+	return output_word
 end
 
 return M
--- lua_neovim_and_normal/neovim_plugin_luajit/ceaser-cipher.nvim/lua/tests/ceaser-cipher/ceaser-cipher_spec.lua
+++ lua_neovim_and_normal/neovim_plugin_luajit/ceaser-cipher.nvim/lua/tests/ceaser-cipher/ceaser-cipher_spec.lua
@@ -1,15 +1,33 @@
 describe("Ceaser cipher tests --", function()
-  before_each(function()
-    -- Run time path is not getting loaded automatically, so modify it before each test
-    -- This will find the first directory named 'lua' and add the directory one level above to vim run time path
-    local path_to_plugin = debug.getinfo(1).source:match("@(.*)[/\\]lua[/\\]"):gsub('"', "")
-    vim.cmd([[set runtimepath+=]] .. path_to_plugin)
-    print(string.format("Adding: %s to neovim runtimepath because plenary tests fail without this", path_to_plugin))
-  end)
+	before_each(function()
+		-- Run time path is not getting loaded automatically, so modify it before each test
+		-- This will find the first directory named 'lua' and add the directory one level above to vim run time path
+		local path_to_plugin = debug.getinfo(1).source:match("@(.*)[/\\]lua[/\\]"):gsub('"', "")
+		vim.cmd([[set runtimepath+=]] .. path_to_plugin)
+		print(string.format("Adding: %s to neovim runtimepath because plenary tests fail without this", path_to_plugin))
+	end)
 
-  it("Require the module", function()
-    local package = require("ceaser-cipher").cipher
-    local type_of_package = type(package)
-    assert.are.same(type_of_package, "function")
-  end)
+	it("Require the module", function()
+		local package = require("ceaser-cipher").cipher
+		local type_of_package = type(package)
+		assert.are.same(type_of_package, "function")
+	end)
+
+	it("Single letter conversion", function()
+		local cipher = require("ceaser-cipher").cipher
+		local output = cipher("A", 2)
+		assert.are.same("C", output)
+	end)
+
+	it("Single letter conversion with wrap", function()
+		local cipher = require("ceaser-cipher").cipher
+		local output = cipher("B", 25)
+		assert.are.same("A", output)
+	end)
+
+	it("Entire word", function()
+		local cipher = require("ceaser-cipher").cipher
+		local output = cipher("ABC", 1)
+		assert.are.same("BCD", output)
+	end)
 end)
